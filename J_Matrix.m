04a3d85a0f60a1e60ebeb80ba352859859f3e7eb
mdcallaham <Mdcallaham@gmail.com>
2016-11-18 15:31:33 -0500

    Update J_Matrix.m

--- J_Matrix.m
+++ J_Matrix.m
@@ -1,9 +1,6 @@
 function [ J,dNdx,dNdy,dNdz ] = J_brick( Epsilon, Eta, Zeta, x, y, z )
-% J_brick takes given Epsilon, Eta, Zeta (doubles), x, y, and z (1x8 vectors)
-% for a brick element and returns J. Epsilon, Eta, and Zeta go from -1 to 1.
-
-% Epsilon is Xi in the notes. EpsilonI, EtaI, and ZetaI are the nodal
-% values in order
+% J matrix. 
+% Epsilon, Eta, Zeta are determined by the location in the brick element
 EpsilonI=[-1 -1 -1 -1 1 1 1 1]; 
 EtaI=[-1 -1 1 1 -1 -1 1 1];
 ZetaI=[1 -1 -1 1 1 -1 -1 1];
@@ -14,13 +11,12 @@ for i=1:8 % iterates over each node
     dNdZeta(i)=(1/8)*ZetaI(i)*(1+Epsilon*EpsilonI(i))*(1+Eta*EtaI(i));
 end
 
-J=[dNdEpsilon; dNdEta; dNdZeta]*[x' y' z']; % assembles derivative and nodal locations matrices and multiplies
-Jp=J^-1; % finds the inverse of J
+J=[dNdEpsilon; dNdEta; dNdZeta]*[x' y' z']; 
+Jp=J^-1; 
 
 for i=1:8
-    Resultant=Jp*[dNdEpsilon(i); dNdEta(i); dNdZeta(i)]; % finds the cartesian derivatives
-    dNdx(i)=Resultant(1); % stores the current nodal x derivative
-    dNdy(i)=Resultant(2); % stores the current nodal y derivative
-    dNdz(i)=Resultant(3); % stores the current nodal z derivative
+    Direction=Jp*[dNdEpsilon(i); dNdEta(i); dNdZeta(i)]; % finds the cartesian derivatives
+    dNdx(i)=Direction(1); % stores the current nodal x derivative
+    dNdy(i)=Direction(2); % stores the current nodal y derivative
+    dNdz(i)=Direction(3); % stores the current nodal z derivative
 end
-
