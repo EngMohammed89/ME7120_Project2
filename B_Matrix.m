64fa1aedcc917ce4668b8d1efd446d8db4ca35c8
mdcallaham <Mdcallaham@gmail.com>
2016-11-18 14:23:09 -0500

    Update B_Matrix.m

--- B_Matrix.m
+++ B_Matrix.m
@@ -1,18 +1,19 @@
-function [ B ] = B_brick( dNdx, dNdy, dNdz )
-% B_brick takes dNdx, dNdy, and dNdz as output from J_brick and calculates
-% B for a brick element with it
-B=zeros(6,24); % Preallocates B
-for i=1:8 % loops for each node of the element
+function [ B ] = B_brick(dNdx, dNdy, dNdz)
+%Builds B Matrix
+%B_brick takes dNdx, dNdy, and dNdz as output from J_brick and calculates
+
+
+B=zeros(6,24); 
+for i=1:8 %Assembles B matrix
     Bi=[dNdx(i) 0       0;
         0       dNdy(i) 0;
         0       0       dNdz(i);
         dNdy(i) dNdx(i) 0;
         0       dNdz(i) dNdy(i);
-        dNdz(i) 0       dNdx(i)]; % builds nodal B matrix
-    j=1+(i-1)*3; % j and k are indices for placing Bi within B correctly
+        dNdz(i) 0       dNdx(i)]; 
+    j=1+(i-1)*3; 
     k=i*3;
-    B(:,j:k)=Bi(:,:); % Stores Bi in the appropriate node location in B
+    B(:,j:k)=Bi(:,:); 
 end
 
 end
-
