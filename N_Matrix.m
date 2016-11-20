698a79f5e8b87f26c05791a6b2136a41f3865b33
mdcallaham <Mdcallaham@gmail.com>
2016-11-20 13:58:05 -0500

    updated

--- N_Matrix.m
+++ N_Matrix.m
@@ -1,13 +1,15 @@
-function [ N ] = N_brick(Epsilon, Eta, Zeta)
-% N_brick uses inputs Epsilon, Eta and Zeta to generate N
+function [ N ] = N_brick(Xi, Eta, Zeta)
+% N_brick uses inputs Xi, Eta and Zeta to generate N
 
-EpsilonI=[-1 -1 -1 -1 1 1 1 1];
-EtaI=[-1 -1 1 1 -1 -1 1 1];
-ZetaI=[1 -1 -1 1 1 -1 -1 1];
+XiI=   [-1 -1 -1 -1  1  1  1  1];
+EtaI=  [-1 -1  1  1 -1 -1  1  1];
+ZetaI= [-1  1  1 -1 -1  1  1 -1];
 N=[]; % creates N matrix
 for i=1:8 % iterates over each node
-    Ni=(1/8)*(1+Epsilon*EpsilonI(i))*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i)); % Calculates each nodal shape function
+    Ni=(1/8)*(1+Xi*XiI(i))*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i)); % Calculates each nodal shape function
     Ne=[Ni Ni Ni]; % Creates a nodal N matrix
     Ne=diag(Ne); % stores the shape function on the diagonal
     N=[N Ne]; % appends the element shape function matrix with the nodal matrix
+end
+
 end
