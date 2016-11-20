46aeb22e556ee6d96b0ead1b472a8be4b1aebfb8
mdcallaham <Mdcallaham@gmail.com>
2016-11-20 13:57:41 -0500

    updated

--- J_Matrix.m
+++ J_Matrix.m
@@ -1,22 +1,22 @@
-function [ J,dNdx,dNdy,dNdz ] = J_brick( Epsilon, Eta, Zeta, x, y, z )
-% J matrix. 
-% Epsilon, Eta, Zeta are determined by the location in the brick element
-EpsilonI=[-1 -1 -1 -1 1 1 1 1]; 
-EtaI=[-1 -1 1 1 -1 -1 1 1];
-ZetaI=[1 -1 -1 1 1 -1 -1 1];
+function [ J,dNdx,dNdy,dNdz ] = J_brick(Xi, Eta, Zeta, x, y, z)
+% J Matrix based on nodal locations in brick of xi, eta, and zeta
 
-for i=1:8 % iterates over each node
-    dNdEpsilon(i)=(1/8)*EpsilonI(i)*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i));
-    dNdEta(i)=(1/8)*EtaI(i)*(1+Epsilon*EpsilonI(i))*(1+Zeta*ZetaI(i));
-    dNdZeta(i)=(1/8)*ZetaI(i)*(1+Epsilon*EpsilonI(i))*(1+Eta*EtaI(i));
+XiI=   [-1 -1 -1 -1  1  1  1  1];
+EtaI=  [-1 -1  1  1 -1 -1  1  1];
+ZetaI= [-1  1  1 -1 -1  1  1 -1];
+
+for i=1:8 
+    dNdEpsilon(i)=(1/8)*XiI(i)*(1+Eta*EtaI(i))*(1+Zeta*ZetaI(i));
+    dNdEta(i)=(1/8)*EtaI(i)*(1+Xi*XiI(i))*(1+Zeta*ZetaI(i));
+    dNdZeta(i)=(1/8)*Zeta(i)*(1+Xi*XiI(i))*(1+Eta*EtaI(i));
 end
 
-J=[dNdEpsilon; dNdEta; dNdZeta]*[x' y' z']; 
-Jp=J^-1; 
+J=[dNdXi; dNdEta; dNdZeta]*[x' y' z']; % assembles derivative and nodal locations matrices and multiplies
+Jp=J^-1;
 
 for i=1:8
-    Direction=Jp*[dNdEpsilon(i); dNdEta(i); dNdZeta(i)]; % finds the cartesian derivatives
-    dNdx(i)=Direction(1); % stores the current nodal x derivative
-    dNdy(i)=Direction(2); % stores the current nodal y derivative
-    dNdz(i)=Direction(3); % stores the current nodal z derivative
+    Resultant=Jp*[dNdXi(i); dNdEta(i); dNdZeta(i)]; %
+    dNdx(i)=Resultant(1); 
+    dNdy(i)=Resultant(2); 
+    dNdz(i)=Resultant(3); 
 end
