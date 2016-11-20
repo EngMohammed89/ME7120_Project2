8253b2aea05d8cab3c99f2442576bee24f241ae1
EngMohammed89 <ingenium_mj@yahoo.com>
2016-11-20 01:45:36 -0500

    Update Shape_function

--- Project2 - Brick Element/Shape_function.m
+++ Project2 - Brick Element/Shape_function.m
@@ -1,12 +1,14 @@
 function [ N ] = Shape_function(Xi_I,Eta_I,Zeta_I,Xi,Eta,Zeta)
 % Shape functions for brick element. 
-Nd=zeros(3,3)
-for i=1:8 % iterates over each node
+
+N=zeros(3,24);
+for i=1:8
     Ni=(1/8)*(1+Xi*Xi_I(i))*(1+Eta*Eta_I(i))*(1+Zeta*Zeta_I(i)); % it finds shape function for each node
-    Nd(i)=[Ni 0 0;
-           0 Ni 0;
-           0 0 Ni]; % Creates diagonal matrix to create N matrix later
+    Nd=[Ni 0 0;
+        0 Ni 0;
+        0 0 Ni]; % Creates diagonal matrix to create N matrix later
+    
+    % Shape function, N
+    N(:,(i*3-2):(i*3))=Nd(:,:); % This assembles 3x24 N matrix 
 end
-% Shape function N
-N=[Nd(1) Nd(2) Nd(3) Nd(4) Nd(5) Nd(6) Nd(7) Nd(8)]; % Assembles 6x24 B matrix
 end
