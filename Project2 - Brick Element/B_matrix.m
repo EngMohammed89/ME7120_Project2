ce2c3b82209c3c08c83f0bf1570b260f24eed8cd
EngMohammed89 <ingenium_mj@yahoo.com>
2016-11-20 01:44:28 -0500

    Update B matrix subfunction

--- Project2 - Brick Element/B_matrix.m
+++ Project2 - Brick Element/B_matrix.m
@@ -1,15 +1,16 @@
-function [B] = B_matrix( dNdx, dNdy, dNdz )
+function [ B ] = B_matrix(dNdx,dNdy,dNdz)
 % Preparing B matrix
-Bi=zeros(6,3);
+
+B=zeros(6,24); 
 for i=1:8 
     % Find Bi at each node
-    Bi(i)=[dNdx(i) 0       0;
-           0       dNdy(i) 0;
-           0       0       dNdz(i);
-           dNdy(i) dNdx(i) 0;
-           0       dNdz(i) dNdy(i);
-           dNdz(i) 0       dNdx(i)];
+    Bi=[dNdx(i)      0        0;
+           0       dNdy(i)    0;
+           0         0      dNdz(i);
+        dNdy(i)    dNdx(i)    0;
+           0       dNdz(i)  dNdy(i);
+        dNdz(i)      0      dNdx(i)];
+% B matrix 
+B(:,(i*3-2):(i*3))=Bi(:,:) % This assembles 6x24 B matrix
 end
-% B matrix
-B=[Bi(1) Bi(2) Bi(3) Bi(4) Bi(5) Bi(6) Bi(7) Bi(8)]; % Assembles 6x24 B matrix
 end
